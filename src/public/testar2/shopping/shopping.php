d7bf15696be46c632955087f6d54a4f7430a0f63
isabelavitoria <isabelavitoria3005@gmail.com>
2023-06-14 17:45:48 -0300

    zd

--- src/public/testar2/shopping/shopping.php
+++ src/public/testar2/shopping/shopping.php
@@ -1,10 +1,14 @@
+<?php 
+    session_start();
+    $_SESSION['hits'] = $_SESSION['hits'] + 1; 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shoping mt incrivel</title>
+    <title>Shopping mt incrivel</title>
 </head>
 <body>
     <?php 
@@ -12,22 +16,24 @@
         $cupomValidos = array('NIVER10','PROMO15');
         $cupomAceito = in_array($cupom, $cupomValidos);
     ?>
-    <h1><a href="shopping.php"><img src=""></a>Shopping Virtual</h1>
+    <h1><a href="index.php"><img src=""></a>Shopping Virtual</h1>
     <ul>
         <li><a href="p1.php<?php echo ($cupomAceito)?'?cupom=' . $cupom:''; ?>">Produto 1</a></li>
         <li><a href="p2.php<?php echo ($cupomAceito)?'?cupom=' . $cupom:''; ?>">Produto 2</a></li>
     </ul>
-    <form action="shopping.php" method="GET">
+    <form action="index.php" method="GET">
         Cupom de desconto: 
         <?php 
             if ($cupomAceito) {
         ?>
         <strong> <?php echo $cupom; ?></strong>
         <img src="" alt="">
+        <br>
         <?php } else { ?>
         <input type="text" name="cupom"><input type="submit" value="Enviar">
-        <?php }; ?>
+        <?php }; 
+            echo 'Esta página foi visualizada ' . $_SESSION['hits'] . ' vezes.';
+        ?>
     </form>
-
 </body>
 </html>
